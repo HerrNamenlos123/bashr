85b5c78d94c03bfaaef1668854fe6fc8dedc3fdc
HerrNamenlos123 <herrnamenlos123@gmail.com>
2023-09-25 00:06:30 +0200

    Implement basic framework and simple echo and clear commands

---
+++ bashr.py
@@ -0,0 +1,113 @@
+import argparse
+import os
+
+args = None
+initialized = False
+
+powershell = ""
+shellscript = ""
+
+class Echo:
+    Bold = "\e[1m"
+    Dim = "\e[2m"
+    Italic = "\e[3m"
+    Underline = "\e[4m"
+    Invert = "\e[7m"
+    Hidden = "\e[8m"
+    Strike = "\e[9m"
+
+    FgBlack = "\e[30m"
+    FgRed = "\e[31m"
+    FgGreen = "\e[32m"
+    FgYellow = "\e[33m"
+    FgBlue = "\e[34m"
+    FgMagenta = "\e[35m"
+    FgCyan = "\e[36m"
+    FgLightGray = "\e[37m"
+    FgGray = "\e[90m"
+    FgLightRed = "\e[91m"
+    FgLightGreen = "\e[92m"
+    FgLightYellow = "\e[93m"
+    FgLightBlue = "\e[94m"
+    FgLightMagenta = "\e[95m"
+    FgLightCyan = "\e[96m"
+    FgWhite = "\e[97m"
+
+    BgBlack = "\e[40m"
+    BgRed = "\e[41m"
+    BgGreen = "\e[42m"
+    BgYellow = "\e[43m"
+    BgBlue = "\e[44m"
+    BgMagenta = "\e[45m"
+    BgCyan = "\e[46m"
+    BgLightGray = "\e[47m"
+    BgGray = "\e[100m"
+    BgLightRed = "\e[101m"
+    BgLightGreen = "\e[102m"
+    BgLightYellow = "\e[103m"
+    BgLightBlue = "\e[104m"
+    BgLightMagenta = "\e[105m"
+    BgLightCyan = "\e[106m"
+    BgWhite = "\e[107m"
+
+def powershell_append(command):
+    global powershell
+    initted()
+    powershell += command
+
+def shellscript_append(command):
+    global shellscript
+    initted()
+    shellscript += command
+
+def init():
+    global args
+    global initialized
+    global powershell
+    global shell
+
+    parser = argparse.ArgumentParser(description='Bashr: A tool to auto-generate powershell and shell scripts')
+    parser.add_argument('--output-folder', dest='output_folder', type=str, help='Output folder to place the generated scripts into', required=False, default="out")
+    args = parser.parse_args()
+    initialized = True
+
+    powershell = ""
+    shell = ""
+
+    powershell_append("")
+
+def initted():
+    if not initialized:
+        raise Exception("Bashr was not initialized. Did you forget to call bashr.init()?")
+
+def echo(message, no_newline=False):
+    initted()
+
+    contains_colors = message.find('\e[') != -1
+
+    message = message.replace('"', '\\"')
+    powershell_msg = message.replace('\e[', '[').replace('\\', '`')
+
+    if contains_colors:
+        message += "\e[0m"
+        powershell_msg += "[0m"
+
+    if no_newline:
+        powershell_append(f'Write-Host "{powershell_msg}" -NoNewline\n')
+        shellscript_append(f'echo -n -e "{message}"\n')
+    else:
+        powershell_append(f'Write-Host "{powershell_msg}"\n')
+        shellscript_append(f'echo -e "{message}"\n')
+
+def clear():
+    initted()
+    powershell_append("Clear-Host\n")
+    shellscript_append("clear\n")
+
+def close():
+    initted()
+    os.makedirs(args.output_folder, exist_ok=True)
+    with open(f'{args.output_folder}/run.ps1', "w") as f:
+        f.write(powershell)
+    with open(f'{args.output_folder}/run.sh', "w") as f:
+        f.write(shellscript)
---
+++ tests/test.py
@@ -0,0 +1,23 @@
+import os, sys      # This boilerplate is just for the tests, since the script is placed in a parent directory
+sys.path.insert(1, os.path.dirname(os.getcwd()))
+
+import bashr
+
+bashr.init()
+
+bashr.clear()
+bashr.echo("Terminal was just cleared")
+
+bashr.echo(bashr.Echo.FgGreen + bashr.Echo.Underline + "Hello " + bashr.Echo.FgYellow + bashr.Echo.BgGray + "World!")
+bashr.echo("Another", no_newline=True)
+bashr.echo(" " + bashr.Echo.Strike + "test")
+bashr.echo("Bold: " + bashr.Echo.Bold + "test")
+bashr.echo("Dim: " + bashr.Echo.Dim + "test")
+bashr.echo("Underline: " + bashr.Echo.Underline + "test")
+bashr.echo("Italic: " + bashr.Echo.Italic + "test")
+bashr.echo("Invert: " + bashr.Echo.Bold + "test")
+bashr.echo("Hidden: " + bashr.Echo.Hidden + "test")
+bashr.echo("Strike: " + bashr.Echo.Strike + "test")
+bashr.echo("This is a \"Quote\" test")
+
+bashr.close()
